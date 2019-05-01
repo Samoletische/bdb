880a25900ff30d9f6e67c319f7db6e2208e1a94a
Dmitriy <samoletische@yandex.ru>
2019-05-01 21:41:26 +0600

    Добавлены две команды: баланс и отчёт по статьям

--- bot.php
+++ bot.php
@@ -2,13 +2,15 @@
 
 require_once("config.php");
 
-$conf = new Conf($server, $dbUser, $dbPassword, $database, $debug_mode);
+$conf = new Conf($server, $dbUser, $dbPassword, $database, $debug_mode , $log_on_db);
 $bot = new Bot($conf);
-$mess = $bot->getMessage('php://input');
+$mess = $bot->getMessage(file_get_contents('php://input'));
 $conf->insertLog('Сообщение: '.$mess, true);
-$answer = $bot->makeAnswer();
-$conf->insertLog('Ответ: '.$answer, true);
-echo $bot->sendAnswer($answer);
+$answers = $bot->makeAnswer();
+foreach($answers as $answer) {
+  $conf->insertLog('Ответ: '.$answer, true);
+  echo $bot->sendAnswer($answer);
+}
 //------------------------------------------------------
 
 ?>
--- config.php
+++ config.php
@@ -4,8 +4,9 @@
 //	$server			- (string) адрес сервера БД
 //	$database		- (string) имя БД
 //	$dbUser			- (string) имя пользователя БД
-//	$dbPassword	- (string) пароль пользователя БД
-//	$debug_mode	- (bool) режим отладки
+//	$dbPassword		- (string) пароль пользователя БД
+//	$debug_mode		- (bool) режим отладки
+//	$log_on_db		- (bool) выводить лог в БД или в стандартный вывод;
 require_once('connect.php');
 
 class Conf {
@@ -14,6 +15,7 @@ class Conf {
 
 	private $db;
 	private $debug_mode;
+	private $log_on_db;
 
 	private $access_token	= "2043ff6465a697a2e91dc7a5ec4bc835f6a16385be662efb90ad8d2557e28a5e9e18e038f2aff293b62f8";
 	private $group_token	= "e3c415542019a47f3bc495c56ee87d4d8009fa3f7656fc112770e587ecca1dbab151f285cadbd7e5f01fd";
@@ -22,11 +24,12 @@ class Conf {
 	private $group_id		= '174086337';
 	private $API_URL		= 'https://api.vk.com/method/';
 	private $version		= '5.50';
-	private $help_mess		= "Команды, которые я понимаю:\n1 'Показать сегодня' - показать все операции за сегодня\n2 'Показать вчера' - показать все операции за вчера\n3 '[сумма (расход с '-'; доход с '+' или без знака)] [статья (можно уникальную часть статьи)] [комментарий]' - введение расходов/доходов\nПосле ввода суммы будет уточнение статьи, если я не понял какая именно была указана\n4 'Ты живой?' - секретная фраза\n5 'Помощь' - вывод этого сообщения";
+	private $help_mess		= "Команды, которые я понимаю:\n1 'Показать [параметр]' - показать операции в зависимости от параметра:\nа) 'сегодня' - все операции за сегодня\nб) 'вчера' - все операции за вчера\n2 '[сумма (расход с '-'; доход с '+' или без знака)] [статья (можно уникальную часть статьи)] [комментарий]' - введение расходов/доходов\nПосле ввода суммы будет уточнение статьи, если я не понял какая именно была указана\n3 'Статьи [часть названия]' - вывод списка всех статей (если не указан параметр) или статей, в которых присутствует указанная часть названия\n4 'Баланс' - просмотр баланса за месяц\n5 'Отчет [параметр]' - отчеты в зависимости от параметра:\nа) 'статьи' - по статьям\n6 'Ты живой?' - секретная фраза\n7 'Помощь' - вывод этого сообщения\nПояснения:\n- в круглых скобках пояснения (сумма)\n- в квадратных скобках необязательный параметр [комментарий]";
 	//------------------------------------------------------
 
-	function __construct($server, $dbUser, $dbPassword, $database, $debug_mode, $debug_url='') {
-		$this->debug_mode = $debug_mode;
+	function __construct($server, $dbUser, $dbPassword, $database, $debug_mode, $log_on_db, $debug_url='') {
+		$this->debug_mode	= $debug_mode;
+		$this->log_on_db	= $log_on_db;
 
 		$this->db = mysqli_connect($server, $dbUser, $dbPassword, $database);
 		if ($this->db)
@@ -54,7 +57,10 @@ class Conf {
 
 	public function insertLog($mess, $debug=false) {
 		if (!($debug && !$this->debug_mode))
-			$this->db->query("INSERT INTO logs(message, debug, dateNow) VALUES('".$mess."', '".($debug ? 1 : 0)."', '".date("Y-m-d H:i:s")."')");
+			if ($this->log_on_db || !$debug)
+				$this->db->query("INSERT INTO logs(message, debug, dateNow) VALUES('".$mess."', '".($debug ? 1 : 0)."', '".date("Y-m-d H:i:s")."')");
+			elseif (!$this->log_on_db)
+				echo $mess;
 	}
 	//------------------------------------------------------
 
@@ -133,10 +139,11 @@ class Bot {
 		// todo:
 		//	вставить проверку на получение содержимого запроса,
 		//	а также обработать содержимое запроса на предмет получения корректных данных
-		$data = json_decode(file_get_contents($input));
+		$data = json_decode($input);
 		$this->mess = $data->object->body;
 		$this->messType = $data->type;
 		$this->userID = $data->object->user_id;
+		$this->insertLog($data->object->user_id);
 		$this->userName = $this->getUserInfo()->response[0]->first_name;
 
 		return $this->mess;
@@ -156,45 +163,47 @@ class Bot {
 
 	public function makeAnswer() {
 		$mess = mb_strtolower($this->mess);
-		$this->answer = '';
+		$this->insertLog($mess);
+		$this->answer = array();
 		//++ команды, не связанные с получением ответа из базы
 		// секретный ответ
 		if ($mess == $this->conf->secret_mess) {
-			$this->answer = "Живее всех живых!!! Спасибо, $this->userName";
+			$this->answer[] = "Живее всех живых!!! Спасибо, $this->userName";
 			return $this->answer;
 		}
 
 		// помощь
 		if ($mess == 'помощь') {
-			$this->answer = $this->conf->getHelpMessage();
+			$this->answer = explode("\n", $this->conf->getHelpMessage());
 			return $this->answer;
 		}
 		//-- команды, не связанные с получением ответа из базы
 
 		//++ проверка настроек пользователя в базе
 		if (!$this->db) {
-			$this->answer = 'Ошибка обработки команды';
+			$this->answer[] = 'Ошибка обработки команды';
 			return $this->answer;
 		}
 
 		// ошибка работы с базой
+		$this->insertLog('SELECT purseID FROM users WHERE userID='.$this->userID);
 		$query = $this->db->query('SELECT purseID FROM users WHERE userID='.$this->userID);
 		if (!$query) {
-			$this->insertLog('(002) Не могу выполнить запрос');
-			$this->answer = 'Ошибка обработки команды';
+			$this->insertLog('(002) Не могу выполнить запрос'.$this->db->error);
+			$this->answer[] = 'Ошибка обработки команды';
 			return $this->answer;
 		}
 		// неизвестный (для бота) пользователь
 		if ($query->num_rows == 0) {
 			$this->insertLog('(003) пользователь : '.$userID.' отсутствует в базе данных');
-			$this->answer = 'Ошибка обработки команды';
+			$this->answer[] = 'Ошибка обработки команды';
 			return $this->answer;
 		}
 		// не настроен кошелёк для пользователя
 		$row = $query->fetch_array();
 		if ($row["purseID"] == 0) {
 			$this->insertLog('(004) для пользователя : '.$userID.' не настроен кошелёк');
-			$this->answer = 'Ошибка обработки команды';
+			$this->answer[] = 'Ошибка обработки команды';
 			return $this->answer;
 		}
 		$purseID = $row["purseID"];
@@ -248,55 +257,196 @@ class Bot {
 			if (count($parts) > 1)
       	$answ_param = $param;
 		}
+
+		// 'баланс'
+		if (($answ_type == 0) && ($command == "баланс")) {
+			$answ_type = 6; // showBalance
+			$answ_param = '';
+		}
+
+		// 'отчет'
+		if (($answ_type == 0)
+			&& (($command == "отчет") || ($command == "отчёт"))) {
+
+			$answ_type = 7; // report
+		  if ((count($parts) > 1)	&& ($param == "статьи"))
+				$answ_param = 1; // accounts
+
+		}
   	//-- другие команды, связанные с ответом из базы
 
 		//++ обработка команд, формирование ответа
 		switch ($answ_type) {
-		    case 1: // insertAmount
-		        $res = $this->insertAmount($parts, $purseID);
-		        if ($res['result'] == "ok")
-	            	$this->answer = $res['message'];
-	            else {
-	            	$this->insertLog('(005) ошибка обработки прихода/расхода: '.$res['message']);
-	            	$this->answer = "ошибка обработки команды";
-	            }
-	            break;
-	        case 2: // showAll
-	            if (($answ_param < 1) || ($answ_param > 2)) {
-	            	$this->insertLog('(006) неверные параметры команды "показать": '.$answ_param);
-	            	$this->answer = "ошибка обработки команды";
-	            }
-	            else {
-		            $res = $this->showAll($answ_param, $purseID);
-		            if ($res['result'] == "ok")
-		            	$this->answer = $res['message'];
-		            else {
-		            	$this->insertLog('(007) ошибка обработки команды "показать": '.$res['message']);
-		            	$this->answer = "ошибка обработки команды";
-		            }
-	            }
-	            break;
-	        case 3: // showReceipt
-	        	break;
-	        case 4: // showExpense
-	        	break;
-					case 5: // showAccounts
-						try {
-							$res = $this->getAccount($answ_param, true);
-							$this->answer = "Перечень статей:\n".$res['message'];
-						}
-						catch (Exception $e) {
-				    	$this->insertLog('(008) ошибка обработки команды "статьи": '.$this->db->error());
-							$this->answer = "ошибка обработки команды";
-				    }
-						break;
+    	case 1: // insertAmount
+        $res = $this->insertAmount($parts, $purseID);
+        if ($res['result'] == "ok")
+        	$this->answer[] = $res['message'];
+        else {
+        	$this->insertLog('(005) ошибка обработки прихода/расхода: '.$res['message']);
+        	$this->answer[] = "ошибка обработки команды";
+        }
+        break;
+      case 2: // showAll
+	      if (($answ_param < 1) || ($answ_param > 2)) {
+	      	$this->insertLog('(006) неверные параметры команды "показать": '.$answ_param);
+	      	$this->answer[] = "неверные параметры команды";
+	      }
+	      else {
+	        $res = $this->showAll($answ_param, $purseID);
+	        if ($res['result'] == "ok")
+	        	$this->answer[] = $res['message'];
+	        else {
+	        	$this->insertLog('(007) ошибка обработки команды "показать": '.$res['message']);
+	        	$this->answer[] = "ошибка обработки команды";
+	        }
+	      }
+	      break;
+      case 3: // showReceipt
+      	break;
+      case 4: // showExpense
+      	break;
+			case 5: // showAccounts
+				try {
+					$res = $this->getAccount($answ_param, true);
+					$this->answer[] = "Перечень статей:\n".$res['message'];
+				}
+				catch (Exception $e) {
+		    	$this->insertLog('(008) ошибка обработки команды "статьи": '.$this->db->error);
+					$this->answer[] = "ошибка обработки команды";
+		    }
+				break;
+			case 6: // showBalance
+				$res = $this->getBalance($purseID);
+				if ($res['result'] == "ok")
+				 	$this->answer[] = $res['message'];
+				 else {
+				 	$this->insertLog('(009) ошибка обработки команды "баланс": '.$res['message']);
+				 	$this->answer[] = "ошибка обработки команды";
+				 }
+				break;
+			case 7: // report
+				if (($answ_param < 1) || ($answ_param > 2)) {
+	      	$this->insertLog('(010) неверные параметры команды "отчёт": '.$answ_param);
+	      	$this->answer[] = "неверные параметры команды";
+	      }
+	      else {
+	        $res = $this->getReport($answ_param, $purseID);
+	        if ($res['result'] == "ok")
+	        	$this->answer = explode("\n", $res['message']);
+	        else {
+	        	$this->insertLog('(011) ошибка обработки команды "отчёт": '.$res['message']);
+	        	$this->answer[] = "ошибка обработки команды";
+	        }
+	      }
+				break;
 		    default:
-		    	$this->answer = $this->conf->getNoCommandMessage();
+		    	$this->answer[] = $this->conf->getNoCommandMessage();
 		}
 		//-- обработка команд, формирование ответа
 
 		return $this->answer;
 	}
+	//------------------------------------------------------
+
+	private function getReport($reportType, $purseID) {
+		$result = array( 'result' => 'ok', 'message' => '' );
+
+		if (!$this->db) {
+			$result['message'] = 'Отсутствует подключение к БД';
+			return $result;
+		}
+
+		try {
+			$periodStart = date('Y-m-1 00:00:00');
+			$periodEnd = date('Y-m-t 23:59:59');
+			$queryStr = "SELECT IFNULL(accounts.title, '') AS account, SUM(purse.amount) AS amount FROM purse INNER JOIN accounts ON accounts.id=purse.account WHERE purseID = $purseID GROUP BY accounts.title ORDER BY amount";
+			$this->insertLog($queryStr."<br/>", true);
+			$query = $this->db->query($queryStr);
+			if (!$query)
+				throw new Exception('');
+			$result['message'] = "Отчёт по статьям за текущий месяц:";
+			while ($row = $query->fetch_array()) {
+				$result['message'] .= "\n".$row['account'].": ".$row['amount'];
+			}
+		}	catch (Exception $e) {
+			$result['result'] = "error";
+			$result['message'] = $this->db->error;
+			$this->insertLog("balance error: ".$e."<br/>", true);
+		}
+
+		return $result;
+	}
+	//------------------------------------------------------
+
+	private function getBalance($purseID) {
+		$result = array( 'result' => 'ok', 'message' => '' );
+
+		if (!$this->db) {
+			$result['message'] = 'Отсутствует подключение к БД';
+			return $result;
+		}
+
+		try {
+			$periodStart = date('Y-m-1 00:00:00');
+			$periodEnd = date('Y-m-t 23:59:59');
+
+			$queryStr = "SELECT SUM(IFNULL(amount, 0)) AS startBalance FROM purse WHERE purseID = $purseID AND period < '".$periodStart."';
+				SELECT SUM(amount) AS income FROM purse WHERE purseID = $purseID AND moition = 1 AND period >= '".$periodStart."' AND period <= '".$periodEnd."';
+				SELECT SUM(amount) AS expense FROM purse WHERE purseID = $purseID AND moition = 0 AND period >= '".$periodStart."' AND period <= '".$periodEnd."'";
+			//$this->insertLog($queryStr."<br/>", true);
+			$query = $this->db->multi_query($queryStr);
+			if (!$query)
+				throw new Exception('');
+
+			$this->insertLog('query passed<br/>', true);
+
+			// startBalance
+			$res = $this->db->store_result();
+			if (!$res)
+				throw new Exception('');
+
+			$this->insertLog('store_result passed<br/>', true);
+
+			$row = $res->fetch_array();
+			$this->insertLog("start balance =".$row['startBalance']."<br/>", true);
+			$startBalance = $row['startBalance'];
+			$result['message'] = "Баланс на начало месяца: ".($startBalance ? $startBalance : 0);
+
+			// income
+			if (!$this->db->next_result())
+				throw new Exception('');
+
+			$res = $this->db->store_result();
+			if (!$res)
+				throw new Exception('');
+
+			$row = $res->fetch_array(); // startBalance
+			$income = $row['income'];
+			$this->insertLog("income =".$row['income']."<br/>", true);
+			$result['message'] .= "\nДоход за месяц: ".$income;
+
+			// expense
+			if (!$this->db->next_result())
+				throw new Exception('');
+
+			$res = $this->db->store_result();
+			if (!$res)
+				throw new Exception('');
+
+			$row = $res->fetch_array(); // startBalance
+			$expense = $row['expense'];
+			$this->insertLog("expense =".$row['expense']."<br/>", true);
+			$result['message'] .= "\nРасход за месяц: ".(-$expense);
+
+			$result['message'] .= "\nБаланс на конец месяца: ".($startBalance + $income + $expense);
+		}	catch (Exception $e) {
+			$result['result'] = "error";
+			$result['message'] = $this->db->error;
+			$this->insertLog("balance error: ".$e."<br/>", true);
+		}
+
+		return $result;
+	} // getBalance
 	//------------------------------------------------------
 
 	private function getAccount($acc, $mayGetAll = false) {
@@ -305,7 +455,7 @@ class Bot {
 		if (($acc == '') && !$mayGetAll)
 			return $result;
 
-		$query = $this->db->query("SELECT id, title FROM accounts WHERE title LIKE '%$acc%'");
+		$query = $this->db->query("SELECT id, title FROM accounts WHERE title LIKE '%$acc%' ORDER BY title");
 		if (!$query)
 			throw new Exception('');
 
@@ -394,7 +544,7 @@ class Bot {
 		}
 	    catch (Exception $e) {
 	    	$result['result'] = "error";
-	    	$result['message ']= $this->db->error();
+	    	$result['message ']= $this->db->error;
 	    }
 
 	    return $result;
@@ -436,20 +586,22 @@ class Bot {
 	        $periodTimeStamp = strtotime($periodStart);
 	        $result['message'] = "Все операции за: ".date('d', $periodTimeStamp).".".date('m', $periodTimeStamp).".".date('Y', $periodTimeStamp);
 	        $message = '';
+	        $summ = 0;
 	        while ($row = $query->fetch_array()) {
 	            $time_date = explode(" ", $row["period"]);
 	            $date = explode("-", $time_date[0]);
 	            $time = explode(":", $time_date[1]);
 
-				$message .= "\n".$count.") ".$time[0].":".$time[1]." (".$row["name"].") ".$row["amount"]." (".$row["comment"].")";
+			$message .= "\n".$count.") ".$time[0].":".$time[1]." (".$row["name"].") ".$row["amount"]." (".$row["comment"].")";
+			$summ += (float) $row["amount"];
 
 	            $count++;
 	        }
-	        $result['message'] .= $message == '' ? "\nнет записей" : $message;
+	        $result['message'] .= $message == '' ? "\nнет записей" : $message."\nИтого: ".$summ;
 	    }
 	    catch (Exception $e) {
 	        $result['result'] = "error";
-	        $result['message'] = $this->db->error();
+	        $result['message'] = $this->db->error;
 	    }
 
 	    return $result;
